92df4d63d8ae59ff15622cbb4d3c95d1a4729ea2
Hew01 <maiphuctam62@gmail.com>
2022-05-30 07:54:52 +0700

    Changed Layout Form TTTK

--- NewProject/FormThongTinTaiKhoan.Designer.cs
+++ NewProject/FormThongTinTaiKhoan.Designer.cs
@@ -107,7 +107,7 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(56, 223);
+            this.label7.Location = new System.Drawing.Point(62, 223);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(98, 18);
             this.label7.TabIndex = 17;
@@ -148,7 +148,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(104, 138);
+            this.label4.Location = new System.Drawing.Point(110, 138);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(50, 18);
             this.label4.TabIndex = 11;
@@ -176,8 +176,9 @@
             // 
             // FormThongTinTaiKhoan
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.Color.Azure;
             this.ClientSize = new System.Drawing.Size(501, 393);
             this.Controls.Add(this.btnExit);
             this.Controls.Add(this.btnXacNhan);
